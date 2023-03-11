7d52f798bea4532df079fde98892e5dece09d8df
Alan7uuu <121037736@upq.edu.mx>
2023-03-10 22:32:12 -0600

    parte logica practica 14

--- PRACTICA14/logica.py
+++ PRACTICA14/logica.py
@@ -1,5 +1,9 @@
 class logica():
+    def __init__(self,s):
+        self.serie=s
+    def registro():
+        x=[]
     def __init__(self):
         listaNO=list()
         listaTitular=list()
-        lista
+        
